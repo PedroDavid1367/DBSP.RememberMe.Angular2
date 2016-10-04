1e4dd8c723a1bd27de8ec23ece52a2b7d0af3be1
David1367 <pedro.571.david@gmail.com>
2016-10-04 12:49:56 -0400

    Working on enhancing the code

--- app/common.services/oidc-token-manager.service.ts
+++ app/common.services/oidc-token-manager.service.ts
@@ -1,34 +1,27 @@
 ﻿require("../js.libs/oidc-token-manager.js");
 declare let OidcTokenManager: any;
 
-import { Injectable} from '@angular/core';
+import { Injectable}  from '@angular/core';
 
 @Injectable()
 export class OidcTokenManagerService {
   private _config = {
     client_id: "remembermeimplicit",
-
-    // redirect_uri: "http://localhost:8888/callback", 
-    // post_logout_redirect_uri: "http://localhost:8888/home",
-    // authority: "http://localhost:1693/identity",   
-    // silent_redirect_uri: "http://localhost:8888/silent-refresh",
+    
+    redirect_uri: "http://localhost:8888/callback", 
+    post_logout_redirect_uri: "http://localhost:8888/home",
+    authority: "http://localhost:1693/identity",   
+    silent_redirect_uri: "http://localhost:8888/silent-refresh",
 
     // redirect_uri: "http://dbsp-rememberme-angular2.azurewebsites.net/callback",
     // post_logout_redirect_uri: "http://dbsp-rememberme-angular2.azurewebsites.net/home",
     // authority: "http://dbsp-rememberme-identity.azurewebsites.net/identity",
     // silent_redirect_uri: "http://dbsp-rememberme-angular2.azurewebsites.net/silent-refresh",
 
-<<<<<<< HEAD
     // redirect_uri: "http://localhost:8888/callback", 
     // post_logout_redirect_uri: "http://localhost:8888/home",
     // authority: "http://localhost/sts/identity",   
     // silent_redirect_uri: "http://localhost:8888/silent-refresh",
-=======
-    redirect_uri: "http://localhost:8888/callback", 
-    post_logout_redirect_uri: "http://localhost:8888/home",
-    authority: "http://localhost/identity",   
-    silent_redirect_uri: "http://localhost:8888/silent-refresh",
->>>>>>> 37becd6f98e4b13e9d3bdfba730f30d5046213f9
 
     response_type: "id_token token",
     scope: "openid profile addresses notesmanagement roles", 
