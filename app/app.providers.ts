4066c95fe5b497387166132685e66793988e9d3a
David1367 <pedro.571.david@gmail.com>
2016-10-06 06:55:50 -0400

    After testing production environment for the Backend

--- app/app.providers.ts
+++ app/app.providers.ts
@@ -6,7 +6,8 @@ import { AutoLinkerService }                   from "./common.services/auto-link
 let $ = require("jquery");
 
 export const APP_PROVIDERS = [
-  provide("BASE_URL", { useValue: "http://localhost:7338/" }),  // RememberMe REST API Application
+  //provide("BASE_URL", { useValue: "http://localhost:7338/" }),  // RememberMe REST API Application (development)
+  provide("BASE_URL", { useValue: "http://localhost:8887/" }),  // RememberMe REST API Application (production)
   provide("$", { useValue: $ }),
   OidcTokenManagerService,
   AutoLinkerService,
